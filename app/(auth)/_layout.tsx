48f29718178b433ff3f80ed750abfeb6e55a4143
du-nt <mahoagray@gmail.com>
2024-08-11 13:44:20 +0900

    init

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -10,8 +10,8 @@ export default function AuthLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="login/index" options={{ headerShown: false }} />
-      <Stack.Screen name="register/index" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="register" options={{ headerShown: false }} />
     </Stack>
   );
 }
